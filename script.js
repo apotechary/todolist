d0db70c03ddc96133185dfffb602b69f8e1888bd
Loza Roba <robahloza@gmail.com>
2020-05-31 20:15:52 -0400

    Added functionality

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
 "use strict";
 
+
+
+function getTaskList(e) {
+    var ul = $("#task-lists");
+    ul.empty();
+
+    var td = e.target;
+    console.log('before', defaultDateElement.classList)
+    defaultDateElement.classList.remove("selected");
+    console.log('after', defaultDateElement.classList)
+    defaultDateElement = td;
+    td.setAttribute("class", "selected");
+
+
+
+    var cellValue = defaultMonth + e.target.innerText;
+    defaultDate = cellValue;
+    console.log('date', cellValue);
+    var data = database[cellValue];
+    if (data === undefined) {
+        database[cellValue] = [];
+    } else {
+
+        for (let i = 0; i < data.length; i++) {
+            var li = createTaskListLi(data[i]);
+            ul.append(li);
+        }
+    }
+}
+
 function addTaskList() {
+
     var task = $("#task-todo").val().trim();
     if (task === "") {
         alert("please enter a task!");
         return;
     }
+
+    if (database[defaultDate] === undefined) {
+        database[defaultDate] = [];
+    }
+    database[defaultDate].push(task);
     var ul = $("#task-lists");
     var li = createTaskListLi(task);
     ul.append(li);
@@ -36,14 +72,14 @@ function createTaskListLi(todoText) {
     li.appendChild(span1);
     li.appendChild(text);
     li.appendChild(span2);
-
     return li;
 
 }
+
 function removeDoneTasks() {
     $(this).closest("li").remove();
-
 }
+
 function strikeThrough(e) {
     console.log("event", e.target);
     var checkbox = e.target;
@@ -55,11 +91,8 @@ function strikeThrough(e) {
         //checkbox.style.textDecoration = 'line-through';
 
     } else {
-
         li.classList.remove("strike");
-
     }
-
 }
 
 function clearField() {
